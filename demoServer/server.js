3b46e2a0119fd1eddb701219c62adbef0cf9f13c
clockwerkz <clockwerkz@gmail.com>
2020-04-23 22:04:11 -0700

    updates with mongodb

--- demoServer/server.js
+++ demoServer/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 let data = require('./data');
+require('dotenv').config(); 
+const User = require('./models/User');
+
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { v4 } = require('uuid');
 //Nodemon NPM website:
@@ -13,66 +17,56 @@ const PORT = process.env.PORT || 3000;
 //     res.send(`<h1>Hello from the ${req.query.name}!</h1>`);
 // });
 
+mongoose.connect(process.env.DB_URI, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true 
+ })
+    .then(() => console.log('MongoDB connected...'))
+    .catch((err) => console.error("Error:",err));
 app.use(bodyParser.json());
 
-app.get('/profiles', function(req,res){
-    return res.status(200).json(data);
-});
 
 app.get('/profile/:id', function(req,res){
-    let profile;
     let id = req.params.id;
-    for (let i=0; i<data.length; i++) {
-        if (data[i].id === id) {
-            profile = data[i];
-        }
-    }
-    if (profile === undefined) {
-        return res.status(400).send("Invalid User Profile Id");
-    } else {
-        return res.status(200).json(profile);
-    }   
+    User.findById(id)
+    .then(user => res.status(200).send(user))
+    .catch(err => res.status(400).send({ err : "User Profile Not Found" }));
 });
 
 app.post('/profile', function(req,res){
-    const person = req.body;
-    person.id = v4();
-    if (person.name !== undefined) {
+    const profile = req.body;
+    if (profile.email !== undefined) {
        //Save new person in database 
-       data.push(person);
-       return res.status(200).send('Successfully added person');
+        const newUser = new User(profile);
+        newUser.save()
+        .then(user => res.status(200).send(user))
+        .catch(err => res.status(400).send({ err }));
     } else {
         return res.status(400).send("Invalid person");
     }
 });
 
-app.put('/profile', function(req,res){
-    let user = req.body;
-    for (let i=0; i<data.length; i++) {
-        if (data[i].id === user.id) {
-            data[i]= user;
-            return res.json({msg:"Profile Updated"});
-        }
-    }
-    return res.status(400).json({error:"Profile not updated"});
+
+app.put('/profile/:id', function(req,res){
+    let id = req.params.id;
+    let profile= req.body;
+    User.findByIdAndUpdate(id, {$set: { ...profile }}, {new: true})
+    .then(user => res.status(200).send(user))
+    .catch(err => res.status(400).send({ err }));
 });
 
 app.delete('/profile/:id', function(req,res){
     const id = req.params.id;
-    if (id===undefined) {
-        return res.send(400);
-    }
-    let index = -1;
-    for (let i=0; i<data.length; i++) {
-        if (data[i].id === id) {
-            index = i;
+    User.findByIdAndDelete(id)
+    .then(user => {
+        if (user!==null) {
+            res.status(200).send({msg : `User id ${id} was successfully deleted.`});
+        } else {
+            res.status(400).send({ err: "User Not Found" });
         }
-    }
-    if (index === -1) {
-        return res.send(400);
-    }
-    data = data.slice(0,index).concat(data.slice(index+1));
-    return res.status(200).json({msg: "Profile successfully deleted"});
+    })
+    .catch(err => res.status(400).send({ err }));
 });
 
 
